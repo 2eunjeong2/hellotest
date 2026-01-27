fcb754b64aa7ac3323858326799de90066996494
olivia <qwert45640@gmail.com>
2026-01-27 16:24:43 +0900

    연습문제1-6

---
+++ test06.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 연습문제 1-6
+
+// 빈칸이 포함된 문자열 "Be Happy!"를 저장할 가장 작은 char형 배열을 선언하고
+// 키보드로부터 입력하는 코드를 작성합니다.
+
+int main(void)
+{
+	char str[10];
+	gets(str);
+
+	return 0;
+}

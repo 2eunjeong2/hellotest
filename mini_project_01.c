7aabb37ccd3ea02b4063250defa82b6defd36f75
olivia <qwert45640@gmail.com>
2026-01-30 11:57:45 +0900

    메뉴 출력

---
+++ mini_project_01.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>	
+
+// 자판기 구현하기
+
+int main(void)
+{
+	int selec_menu = 0;
+
+	printf("메뉴\n");
+	printf("1. coffee\n");
+	printf("2. drink\n");
+	printf("3. tea\n");
+	printf("4. snack\n");
+
+	printf("메뉴를 선택하세요 : ");
+	scanf("%d", &selec_menu);
+
+	return 0;
+}

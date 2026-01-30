e2309e87fb139827b47910c354cab4abe1e7a86c
olivia <qwert45640@gmail.com>
2026-01-30 15:24:42 +0900

    결제 수단 선택

--- mini_project_01.c
+++ mini_project_01.c
@@ -4,18 +4,58 @@
 
 // 자판기 구현하기
 
+//int payments(int pay);
+
 int main(void)
 {
-	int selec_menu = 0;
+	// 메뉴, 가격, 결제 수단 배열 
+	char* menus[] = { "카다이프","피스타치오", "초콜릿", "버터" };
+	int* prices[] = { 5000, 8000, 3000, 4000 }; 
+	char* payments[] = {"현금", "카드"};
 
-	printf("메뉴\n");
-	printf("1. coffee\n");
-	printf("2. drink\n");
-	printf("3. tea\n");
-	printf("4. snack\n");
+	// 선택 변수
+	char selec_menu[] = "";
+	char selec_pay[20] = "";
 
-	printf("메뉴를 선택하세요 : ");
-	scanf("%d", &selec_menu);
+	// 메뉴 출력
+	printf("========== 재료 ==========\n");
+	for (int i = 0; i < 4; i++)
+	{
+		printf("%d. %s : %d원 \n", i+1, menus[i], prices[i]);
+	}
 
+	printf("=========================\n");
+
+	// 메뉴 선택
+	printf("상품을 선택해주세요 : ");
+	scanf("%s", &selec_menu);
+
+	printf("\n");
+
+	// 결제 수단
+	while (1) {
+		printf("결제 방법을 선택해주세요(현금, 카드) : ");
+		scanf("%s", &selec_pay);
+
+		// 수단 별 출력 문구
+		if (strcmp(selec_pay, "현금") == 0)
+		{
+			printf("%d원을 넣어주세요.\n", prices[0]);
+		}
+		else if (strcmp(selec_pay, "카드") == 0)
+		{
+			printf("카드를 리더기에 꽂아주세요.\n");
+		}
+		else
+		{
+			printf("다시 선택해주세요.\n");
+			continue;
+		}
+		break;
+	}
+
+
+
+	
 	return 0;
 }
